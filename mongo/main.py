91f688fc7ced6f466f37ba2c972623c1594a263e
Nicholas Johnson <nick@nicholastjohnson.com>
2014-11-06 15:42:14 -0500

    added mongo project

---
+++ mongo/main.py
@@ -0,0 +1,16 @@
+__author__ = 'Nick'
+
+import bottle
+import pymongo
+
+
+@bottle.route('/')
+def index():
+    connection = pymongo.MongoClient('localhost', 27017)
+    db = connection.test
+    name = db.names
+    item = name.find_one()
+
+    return '<b>Hello %s!</b>' % item['name']
+
+bottle.run(host='localhost', port=8080)
